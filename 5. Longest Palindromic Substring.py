bac3db100c4ec0b55c2aa04064fb8697a45c2a55
xin <xin.miao.uta@gmail.com>
2019-08-03 20:59:32 -0700

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -26,3 +26,27 @@ class Solution(object):
 #                     max_length=k+1
                     
 #         return s[start:start+max_length]
+        if len(s)<2:
+            return s
+
+        if s[0]==s[1]:
+            rt_l,rt_r=0,1
+        else:    
+            rt_l,rt_r=0,0
+        
+
+        for i in range(len(s)-1):
+            l,r=self.check(s,i,i)
+            if r-l+1>rt_r-rt_l:
+                rt_l,rt_r=l,r
+            l,r=self.check(s,i,i+1)
+            if r-l+1>rt_r-rt_l:
+                rt_l,rt_r=l,r
+        return s[rt_l:rt_r+1]
+    def check(self,s,l,r):
+        while l>=0 and r<=(len(s)-1) and s[l]==s[r]:
+    
+                l-=1
+                r+=1
+ 
+        return l+1,r-1
