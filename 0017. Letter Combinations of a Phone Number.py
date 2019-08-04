2ea169305cb739dad207e43342676468be64bf55
xin <xin.miao.uta@gmail.com>
2019-08-04 16:45:31 -0700

    Update 0017. Letter Combinations of a Phone Number.py

--- 0017. Letter Combinations of a Phone Number.py
+++ 0017. Letter Combinations of a Phone Number.py
@@ -1,3 +1,21 @@
+# class Solution(object):
+#     def letterCombinations(self, digits):
+#         """
+#         :type digits: str
+#         :rtype: List[str]
+#         """
+#         lookup={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno",
+#                 "7":"pqrs","8":"tuv","9":"wxyz"}
+#         if digits=="":
+#             return []
+#         ans=[""]
+#         for i in digits:
+#             temp=[]
+#             for j in ans:
+#                 temp+=[j+x for x in lookup[i]]
+#             ans=temp
+            
+#         return ans
 class Solution(object):
     def letterCombinations(self, digits):
         """
@@ -6,13 +24,20 @@ class Solution(object):
         """
         lookup={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno",
                 "7":"pqrs","8":"tuv","9":"wxyz"}
-        if digits=="":
-            return []
-        ans=[""]
-        for i in digits:
-            temp=[]
-            for j in ans:
-                temp+=[j+x for x in lookup[i]]
-            ans=temp
+        
+    
+        
+        
+        def backtrack(combination,next_digit):
+            if len(next_digit)==0:
+                output.append(combination)
+            else:
+                for i in lookup[next_digit[0]]:
+                    backtrack(combination+i,next_digit[1:])
+
             
-        return ans
+        output=[]
+        
+        if digits:
+            backtrack("",digits)
+        return output
