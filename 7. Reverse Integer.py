7b6b4cee3f2d4fac579b44b3ac885add5c88796a
xin <xin.miao.uta@gmail.com>
2019-08-03 21:37:47 -0700

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        
+#         sign=-1 if x<0 else 1
+#         ans=0
+#         x=abs(x)
+#         while x!=0:
+#             ans=ans*10+x%10
+#             x=x/10
+         
+#         ans=ans*sign
+#         if ans>-2**31 and ans<(2**31-1):
+#             return ans
+#         else:
+#             return 0
+
+        
         if x<0 :
             ans=-int(str(-x)[::-1])
         else:
