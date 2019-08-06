bb5d211bb6ce7af91805fdabebfd78297cd97c81
xin <xin.miao.uta@gmail.com>
2019-08-05 20:37:15 -0700

    Update 0031. Next Permutation.py

--- 0031. Next Permutation.py
+++ 0031. Next Permutation.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+#         if len(nums)<=1:
+#             return 
+#         pt=len(nums)-2
+#         for i in range(len(nums)-1):
+#             if nums[pt]<nums[pt+1]:
+#                 pt2=len(nums)-1
+#                 while 1:
+#                     if nums[pt2]>nums[pt]:
+#                         nums[:]=nums[:pt]+[nums[pt2]]+nums[pt2+1:][::-1]+\
+#                                 [nums[pt]]+nums[pt+1:pt2][::-1]
+#                         return
+#                     pt2-=1
+#             pt-=1
+            
+#         nums[:]=nums[::-1]
+#         return 
+        
+
         if len(nums)<=1:
             return 
         pt=len(nums)-2
@@ -12,12 +30,14 @@ class Solution(object):
                 pt2=len(nums)-1
                 while 1:
                     if nums[pt2]>nums[pt]:
-                        nums[:]=nums[:pt]+[nums[pt2]]+nums[pt2+1:][::-1]+\
-                                [nums[pt]]+nums[pt+1:pt2][::-1]
+                        print pt2
+                        temp=nums[pt]
+                        nums[pt]=nums[pt2]
+                        nums[pt2]=temp
+                        nums[pt+1:]=nums[pt+1:][::-1]
                         return
                     pt2-=1
             pt-=1
             
         nums[:]=nums[::-1]
         return 
-        
